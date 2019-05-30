b05e7a182cdd12336fc9382349e078d0ae3478b4
David Ruiz <davidl.ruizc@gmail.com>
2019-05-30 11:39:10 -0500

    Importing the header

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text} from 'react-native';
+import {
+  Platform,
+  StyleSheet,
+  Text
+} from 'react-native';
+// Sections
 import Home from './src/scenes/containers/home'
+import Header from './src/sections/components/header'
 
-const bntColor = Platform.select({
+// Add different color according to the platform
+const txtColor = Platform.select({
   ios: 'green',
-  android: 'yellow'
+  android: 'purple'
 })
 
 type Props = {};
@@ -20,15 +19,19 @@ export default class App extends Component<Props> {
   render() {
     return (
       <Home>
+        <Header>
+          <Text>NAVIGATION</Text>
+        </Header>
         <Text style={styles.text}>Welcome</Text>
       </Home>
     )
   }
 }
 
+// Styles
 const styles = StyleSheet.create({
   text: {
-    color: bntColor
+    color: txtColor
   }
 })
 
