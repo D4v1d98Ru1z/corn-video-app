9d913e2a57bbb2781b3b2c9095cf2fe840da3be5
David Ruiz <davidl.ruizc@gmail.com>
2019-06-03 12:44:34 -0500

    Creating a utils for call the API

---
+++ utils/api.js
@@ -0,0 +1,15 @@
+const BASE_API = 'https://yts.am/api/v2/'
+
+class Api {
+  /**
+   * Get the suggestion from the API
+   * @param {number} id 
+   */
+  async getSuggestion(id) {
+    const query = await fetch(`${BASE_API}movie_suggestions.json?movie_id=${id}`)
+    const { data } = await query.json()
+    return data.movies
+  }
+}
+
+export default new Api()
