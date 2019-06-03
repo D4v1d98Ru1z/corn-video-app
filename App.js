a67d24121e06558d71625915e9af8f8451541194
David Ruiz <davidl.ruizc@gmail.com>
2019-06-03 12:44:55 -0500

    Importing the APi into the project and sending the data by props

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ import {
 import Home from './src/scenes/containers/home'
 import Header from './src/sections/components/header'
 import SuggestionList from './src/videos/containers/suggestion-list'
+// API
+import API from './utils/api'
 
 // Add different color according to the platform
 const txtColor = Platform.select({
@@ -17,6 +19,16 @@ const txtColor = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  state = {
+    suggestionList: []
+  }
+  async componentDidMount() {
+    const movies = await API.getSuggestion(10)
+    console.log(movies)
+    this.setState({
+      suggestionList: movies
+    })
+  }
   render() {
     return (
       <Home>
@@ -24,7 +36,9 @@ export default class App extends Component<Props> {
           <Text>NAVIGATION</Text>
         </Header>
         <Text style={styles.text}>Welcome</Text>
-        <SuggestionList />
+        <SuggestionList 
+          list={this.state.suggestionList}
+        />
       </Home>
     )
   }
